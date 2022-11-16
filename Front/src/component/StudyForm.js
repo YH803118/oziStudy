b7c9b4a81833d2c144b7263caac2b7d8407d5048
YH803118 <dudghks1045@naver.com>
2022-11-16 12:56:23 +0900

    TopMenu+css

--- Front/src/component/StudyForm.js
+++ Front/src/component/StudyForm.js
@@ -1,5 +1,6 @@
+import "./StudyForm.css";
 function StudyForm() {
-  return <div>스터디폼</div>;
+  return <div className="StudyForm">스터디폼</div>;
 }
 
 export default StudyForm;
--- Front/src/component/TopMenu.js
+++ Front/src/component/TopMenu.js
@@ -1,5 +1,22 @@
-function TopMenu() {
-  return <div>TopMenu(로그인)</div>;
+import "./TopMenu.css";
+
+function TopMenu({ login }) {
+  return (
+    <div className="TopMenu">
+      {login ? (
+        <div className="TopMenuItem">
+          {/* <img /> 프로플사진*/}
+          <button>유저이름</button>
+        </div>
+      ) : (
+        <form className="TopMenuItem">
+          아이디 <input type="text" name="loginId" className="loginInput" /> |
+          비밀번호 <input type="text" name="loginPw" className="loginInput" /> |{" "}
+          <button type="submit">로그인</button>
+        </form>
+      )}
+    </div>
+  );
 }
 
 export default TopMenu;
