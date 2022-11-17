5154cb7a6957e6b9375f58d2589ed1685fb8f251
marrtil@naver.com <marrtil@naver.com>
2022-11-17 12:53:03 +0900

    api 몇개 일단 추가

--- Front/src/api.js
+++ Front/src/api.js
@@ -12,3 +12,32 @@ export async function getUser({ userId, password }) {
     return false;
   }
 }
+
+export async function joinMember(member) {
+  const res = await fetch(`htttp://localhost:3000/api/members`, {
+    method: "POST",
+    body: member,
+  });
+  if (!res) throw new Error("회원등록에 실패하였습니다");
+  const body = await res.json();
+  return body;
+}
+
+export async function modMember(userId, member) {
+  const res = await fetch(`htttp://localhost:3000/api/members/${userId}`, {
+    method: "PUT",
+    body: member,
+  });
+  if (!res) throw new Error("회원정보 수정에 실패하였습니다");
+  const body = await res.json();
+  return body;
+}
+
+export async function delMember(userId) {
+  const res = await fetch(`htttp://localhost:3000/api/members/${userId}`, {
+    method: "DELETE",
+  });
+  if (!res) throw new Error("회원 삭제에 실패하였습니다");
+  const body = await res.json();
+  return body;
+}
