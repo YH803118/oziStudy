85fcd3357082f8182aa49ac6ff8445fec5716968
YH803118 <dudghks1045@naver.com>
2022-12-13 14:18:38 +0900

    api url set

--- Front/src/api.js
+++ Front/src/api.js
@@ -1,5 +1,6 @@
+const URL = "https://ozistudy.herokuapp.com";
 export async function getUser({ userId, password }) {
-  const res = await fetch(`http://localhost:3000/api/members/${userId}`);
+  const res = await fetch(`${URL}/api/members/${userId}`);
   if (!res) throw new Error("회원 조회에 실패했습니다");
   const body = await res.json();
 
@@ -13,7 +14,7 @@ export async function getUser({ userId, password }) {
 }
 
 export async function getUserInfo(userId) {
-  const res = await fetch(`http://localhost:3000/api/members/${userId}`);
+  const res = await fetch(`${URL}/api/members/${userId}`);
   if (!res) throw new Error("회원 조회에 실패했습니다");
   const body = await res.json();
 
@@ -23,7 +24,7 @@ export async function getUserInfo(userId) {
 export async function joinMember(formData) {
   console.log(formData);
   console.log(JSON.stringify(formData));
-  const response = await fetch(`http://localhost:3000/api/members/`, {
+  const response = await fetch(`${URL}/api/members/`, {
     method: "POST",
     body: formData,
   });
@@ -36,7 +37,7 @@ export async function joinMember(formData) {
 
 export async function modMember(userId, member) {
   console.log("modMember api");
-  const res = await fetch(`http://localhost:3000/api/members/${userId}`, {
+  const res = await fetch(`${URL}/api/members/${userId}`, {
     method: "PUT",
     // headers: { "Content-type": "multipart/form-data" },
     body: member,
@@ -50,7 +51,7 @@ export async function modMember(userId, member) {
 }
 
 export async function modTable(id, table) {
-  const res = await fetch(`http://localhost:3000/api/tables/${id}`, {
+  const res = await fetch(`${URL}/api/tables/${id}`, {
     method: "PUT",
     body: table,
   });
@@ -60,7 +61,7 @@ export async function modTable(id, table) {
 }
 
 export async function delMember(userId) {
-  const res = await fetch(`http://localhost:3000/api/members/${userId}`, {
+  const res = await fetch(`${URL}/api/members/${userId}`, {
     method: "DELETE",
   });
   if (!res) throw new Error("회원 삭제에 실패하였습니다");
@@ -69,21 +70,21 @@ export async function delMember(userId) {
 }
 
 export async function getStudyList() {
-  const res = await fetch(`http://localhost:3000/api/tables`);
+  const res = await fetch(`${URL}/api/tables`);
   if (!res) throw new Error("스터디조회실패!");
   const body = await res.json();
   return body;
 }
 
 export async function getStudy(id) {
-  const res = await fetch(`http://localhost:3000/api/tables/${id}`);
+  const res = await fetch(`${URL}/api/tables/${id}`);
   if (!res) throw new Error("스터디조회실패!");
   const body = await res.json();
   return body;
 }
 
 export async function getMyStudy(id) {
-  const res = await fetch(`http://localhost:3000/api/tables/myStudy/${id}`);
+  const res = await fetch(`${URL}/api/tables/myStudy/${id}`);
   if (!res) throw new Error("스터디조회실패!");
   const body = await res.json();
   console.log(body);
@@ -91,7 +92,7 @@ export async function getMyStudy(id) {
 }
 
 export async function regiStudy(formData) {
-  const response = await fetch(`http://localhost:3000/api/tables/`, {
+  const response = await fetch(`${URL}/api/tables/`, {
     method: "POST",
     body: formData,
   });
@@ -103,16 +104,14 @@ export async function regiStudy(formData) {
 }
 
 export async function search(searchText) {
-  const res = await fetch(
-    `http://localhost:3000/api/tables/search/${searchText}`
-  );
+  const res = await fetch(`${URL}/api/tables/search/${searchText}`);
   if (!res) throw new Error("검색대작전 대실패!");
   const body = await res.json();
   return body;
 }
 
 export async function delStudy(id) {
-  const res = await fetch(`http://localhost:3000/api/tables/${id}`, {
+  const res = await fetch(`${URL}/api/tables/${id}`, {
     method: "DELETE",
   });
   if (!res) throw new Error("스터디 삭제에 실패하였습니다");
@@ -121,14 +120,14 @@ export async function delStudy(id) {
 }
 
 export async function getComment(studyId) {
-  const res = await fetch(`http://localhost:3000/api/comments/${studyId}`);
+  const res = await fetch(`${URL}/api/comments/${studyId}`);
   if (!res) throw new Error("댓글검색대작전 대실패!");
   const body = await res.json();
   return body;
 }
 
 export async function writeComment(formData) {
-  const response = await fetch(`http://localhost:3000/api/comments/`, {
+  const response = await fetch(`${URL}/api/comments/`, {
     method: "POST",
     body: formData,
   });
@@ -140,7 +139,7 @@ export async function writeComment(formData) {
 }
 
 export async function delComment(id) {
-  const res = await fetch(`http://localhost:3000/api/comments/${id}`, {
+  const res = await fetch(`${URL}/api/comments/${id}`, {
     method: "DELETE",
   });
   if (!res) throw new Error("댓글 삭제에 실패하였습니다");
@@ -149,7 +148,7 @@ export async function delComment(id) {
 }
 
 export async function modComment(id, comment) {
-  const res = await fetch(`http://localhost:3000/api/comments/${id}`, {
+  const res = await fetch(`${URL}/api/comments/${id}`, {
     method: "PUT",
     body: comment,
   });
@@ -160,7 +159,7 @@ export async function modComment(id, comment) {
 
 export async function getImage(image) {
   console.log("getImage");
-  const res = await fetch(`http://localhost:3000/api/image/${image}`);
+  const res = await fetch(`${URL}/api/image/${image}`);
   const body = await res.json();
   return body;
 }
