76efcc24b923a0240e7f88d3f9187412639dbece
YH803118 <dudghks1045@naver.com>
2022-12-15 11:56:28 +0900

    host-name

--- config/config.js
+++ config/config.js
@@ -17,7 +17,7 @@ module.exports = {
     username: process.env.DB_USERNAME,
     password: process.env.DB_PASSWORD,
     database: process.env.DB,
-    host: process.env.HOST,
+    host: process.env.HOST_NAME,
     dialect: "mysql",
   },
 };
