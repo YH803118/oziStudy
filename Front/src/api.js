4bc8a5701e8a653ac605b6c5ed3cbf373b2e11ba
marrtil@naver.com <marrtil@naver.com>
2022-12-15 14:17:54 +0900

    실험 되돌리기

--- Front/src/api.js
+++ Front/src/api.js
@@ -71,12 +71,7 @@ export async function delMember(userId) {
 
 export async function getStudyList() {
   console.log("getStudyList - before fetch");
-  const res = await fetch(`${URL}/api/tables`, {
-    headers: {
-      Accept: "application / json",
-    },
-    method: "GET",
-  });
+  const res = await fetch(`${URL}/api/tables`);
   console.log("getStudyList - after fetch");
   if (!res) throw new Error("스터디조회실패!");
   const body = await res.json();
